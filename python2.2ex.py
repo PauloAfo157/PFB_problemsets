8dd3296e7cf0cb2336afc56c8473758258d10511
PauloAfo157 <paulorenanafo@usp.br>
2023-09-12 15:33:39 -0300

    python2.2ex.py

---
+++ python2.2ex.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+a = 75
+b = -13
+c = 0 
+
+if a < 50 and a > 0:
+    print(a,'it is an even number that is smaller than 50')
+elif a > 50:
+    print(a,'it is larger than 50')
+elif a < 0:
+    print(a,'negative')
+else:
+    print(a,'is 0')
+
+if b < 50 and b > 0:
+    print(b,'it is an even number that is smaller than 50')
+elif b > 50:
+    print(b,'it is larger than 50')
+elif b < 0:
+    print(b,'negative')
+else:
+    print(b,'is 0')
+
+if c < 50 and c > 0:
+    print(c,'it is an even number that is smaller than 50')
+elif c > 50:
+    print(c,'it is larger than 50')
+elif c > 0:
+    print(c,'negative')
+else:
+    print(c,'is 0')
+
+
