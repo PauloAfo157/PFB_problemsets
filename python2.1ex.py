fc58edfa4eca2f0c99dea10ade9e1a7e255830d9
PauloAfo157 <paulorenanafo@usp.br>
2023-09-12 12:06:49 -0300

    python2ex.py

---
+++ python2.1ex.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+a = bool('')
+b = bool(0)
+c = bool(0.0)
+d = bool(False)
+e = bool('0')
+f = bool(True)
+g = bool(26)
+h = bool('True')
+i = bool('False')
+
+if a is True:
+    print('True')
+else:
+    print('Not true')
+
+if b is True:
+    print('True')
+else:
+    print('Not true')
+
+if c is True:
+    print('True')
+else:
+    print('Not true')
+
+if d is True:
+    print('True')
+else:
+    print('Not true')
+
+if e is True:
+    print('True')
+else:
+    print('Not true')
+
+if f is True:
+    print('True')
+else:
+    print('Not true')
+
+if g is True:
+    print('True')
+else:
+    print('Not true')
+
+if h is True:
+    print('True')
+else:
+    print('Not true')
+
+if i is True:
+    print('True')
+else:
+    print('Not true')
